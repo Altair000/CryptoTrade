9e64c8726a220e892969ad8401ab4101f01c9f2b
Altair000 <raydiel_e@icloud.com>
2025-06-09 20:34:37 -0400

    Delete API/app directory

--- API/app/config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-class Config:
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    JWT_SECRET = os.getenv("JWT_SECRET")
-    ADMIN_WALLET = os.getenv("ADMIN_WALLET")
